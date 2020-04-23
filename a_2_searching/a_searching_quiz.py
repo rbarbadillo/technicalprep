8286f204d674cc1afa2eb03556eb604c89a0fc70
Raquel Barbadillo <mrbarbadillo@gmail.com>
2020-04-23 17:13:10 +0200

    Algorithms: BFS and DFS

---
+++ a_2_searching/a_searching_quiz.py
@@ -0,0 +1,17 @@
+# If you know a solution is not far from the root of the tree:
+#  Breadth first search (BFS)
+
+# If the tree is very deep and solutions are rare:
+# BFS. DFS would take a really long time. There are also memory concerns with BFS.
+
+# //If the tree is very wide:
+# // Depth first search (DFS). A BFS would need too much memory.
+
+# //If solutions are frequent but located deep in the tree:
+# // DFS.
+
+# //Determining whether a path exists between two nodes:
+# // DFS
+
+# //Finding the shortest path:
+# // BFS
